4ec9fa10b0acfd5c5963e7aa9647ff2c9466b1f4
Thiago Aramizo Ribeiro <aramizot@users.noreply.github.com>
2022-08-08 22:41:41 -0300

    Luzes de fundo

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const botoes = document.querySelectorAll(".tecla")
+const teclado = document.querySelector(".teclado")
+const marca = document.querySelector("h1")
 
 botoes.forEach( (botao, index) => {
     botao.addEventListener( "click" , () => {
@@ -6,8 +8,28 @@ botoes.forEach( (botao, index) => {
         const som = document.querySelector( classeSom )
         const somTocando = som.cloneNode(true)
         somTocando.play()
+        teclado.classList.add("teclado-ativo")
+        marca.classList.add("marca-ativa")
+        setTimeout(() => {
+            teclado.classList.remove("teclado-ativo")
+            marca.classList.remove("marca-ativa")
+        }, 150)
         somTocando.addEventListener( 'ended' , () => {
             somTocando.remove()
         })
     })
 })
+
+var tocar = false
+
+document.querySelector("#tocar").addEventListener("click", () => {
+    const botao_pom = document.querySelector(".tecla_pom")  
+    tocar = !tocar   
+    if( tocar ){
+        setInterval(() => {
+            botao_pom.click()
+        }, 1000)
+    } else return false
+})
+    
+
